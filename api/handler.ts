0ea93d7f355ca5ddf841305da4ddc3bb2237a5e5
Eric Adams <adams.ea@gmail.com>
2022-08-22 12:07:23 -0400

    Initial commit

---
+++ api/handler.ts
@@ -0,0 +1,17 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
+
+export const hello: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  return Promise.resolve(response);
+}
